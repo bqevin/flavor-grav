4f44dad70e3ff6a33798b714647db56b5025ff39
Kevin Barasa <bkevin001@yahoo.com>
2016-04-09 09:57:58 +0300

    More features added

---
+++ cache/compiled/files/6461bf751f4b6a95a392a0368e4b3a2f.yaml.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'C:/xampp/htdocs/projects/grav/user/accounts/kev_barasa.yaml',
+    'modified' => 1459017475,
+    'data' => [
+        'email' => 'bkevin001@yahoo.com',
+        'fullname' => 'Kevin Barasa',
+        'title' => 'Admin',
+        'state' => 'enabled',
+        'access' => [
+            'admin' => [
+                'login' => true,
+                'super' => true
+            ],
+            'site' => [
+                'login' => true
+            ]
+        ],
+        'hashed_password' => '$2y$10$p.yXjcOS5G33cb0YartDrudiupkL1KD45RpRpRCs57UacIbZ3Qp2y'
+    ]
+];
---
+++ cache/compiled/files/f38b1bb6b0ce19fdf7ec1567cd30add4.yaml.php
@@ -0,0 +1,40 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'C:/xampp/htdocs/projects/grav/user/themes/giansi/blueprints.yaml',
+    'modified' => 1458846728,
+    'data' => [
+        'name' => 'Gravstrap Theme',
+        'version' => '1.0.5',
+        'description' => 'Gravstrap Theme helps you to start a new Grav CMS site with Bootstrap support and several ready to use modules. It is also perfect to start a new Bootstrap custom theme, to create your unique design.',
+        'icon' => 'picture-o',
+        'author' => [
+            'name' => 'Giansimon Diblas',
+            'email' => 'info@diblas.net',
+            'url' => 'http://diblas.net'
+        ],
+        'homepage' => 'https://github.com/giansi/gravstrap-theme',
+        'demo' => 'http://gravstrap.diblas.net/',
+        'keywords' => 'gravstrap, bootstrap, theme, responsive, html5, css3, jquery',
+        'bugs' => 'https://github.com/giansi/gravstrap-theme/issues',
+        'license' => 'MIT',
+        'dependencies' => [
+            0 => 'gravstrap',
+            1 => 'shortcode-core',
+            2 => 'langswitcher',
+            3 => 'blog-injector',
+            4 => 'simplesearch',
+            5 => 'feed',
+            6 => 'relatedpages',
+            7 => 'pagination',
+            8 => 'taxonomylist',
+            9 => 'archives',
+            10 => 'breadcrumbs',
+            11 => 'simple_form',
+            12 => 'highlight'
+        ],
+        'form' => [
+            'validation' => 'loose'
+        ]
+    ]
+];
